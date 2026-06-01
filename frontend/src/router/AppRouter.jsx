004b1640a12064eda5839aedf48298af4fe2a894
Valk_sanx <hvfranco@uniboyaca.edu.co>
2026-05-31 22:46:05 -0500

    arreglo navbar y footer

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -53,9 +53,12 @@ const DashboardLayout = () => {
       <main className="flex-1 w-full max-w-(--size-xl) mx-auto p-4 sm:p-6 lg:p-8">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 };
+
+
 
 const RouterContent = () => {
   return (
